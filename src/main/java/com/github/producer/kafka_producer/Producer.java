1ca6872436e0d484b3b75518a39d768d1a689298
avijit90 <sepiaavi@gmail.com>
2019-10-03 00:39:16 +0800

    Changed to a more spring-ified implementation using kafka-template.

--- src/main/java/com/github/producer/kafka_producer/Producer.java
+++ src/main/java/com/github/producer/kafka_producer/Producer.java
@@ -1,12 +1,7 @@
 package com.github.producer.kafka_producer;
 
 import com.github.producer.model.Bus;
-import com.github.producer.model.BusSerializer;
 import com.github.producer.model.BusServiceResponse;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.Properties;
 
 import static java.text.MessageFormat.format;
 import static java.util.Arrays.asList;
@@ -27,25 +20,11 @@ public class Producer {
 
     final Logger LOG = LoggerFactory.getLogger(Producer.class);
 
-    @Value("${app.kafka.bootstrapServer: localhost:9092}")
-    private String bootstrapServer;
-
     @Value("${app.kafka.topicName: first_topic}")
     private String topicName;
 
-    private KafkaProducer<String, Bus> producer;
-
     @Autowired
     KafkaTemplate<String, Bus> kafkaTemplate;
-
-    @PostConstruct
-    private void buildProperties() {
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, BusSerializer.class.getName());
-        producer = new KafkaProducer<>(properties);
-    }
 
     public void publish(BusServiceResponse messageToPublish) {
 
@@ -55,15 +34,14 @@ public class Producer {
 
             messageToPublish.getServices().stream().forEach(service -> {
 
-                String key = join(asList(busStopCode, service.getServiceNo(), "B"), "_");
+                String prefix = join(asList(busStopCode, service.getServiceNo(), "B"), "_");
 
                 List<Bus> buses = asList(service.getNextBus(), service.getNextBus2(), service.getNextBus3());
                 buses.stream().forEach(bus -> {
 
-                    ProducerRecord<String, Bus> record =
-                            new ProducerRecord<>(topicName, key.concat(Integer.toString(buses.indexOf(bus))), bus);
+                    String key = prefix.concat(Integer.toString(buses.indexOf(bus)));
 
-                    producer.send(record, (recordMetadata, e) -> {
+                   /* kafkaTemplate.send(record, (recordMetadata, e) -> {
                         if (e == null) {
                             LOG.info("Received new metadata");
                             LOG.info(format("Topic : {0}", recordMetadata.topic()));
@@ -72,18 +50,17 @@ public class Producer {
                             LOG.info(format("Timestamp : {0}", recordMetadata.timestamp()));
                         } else
                             LOG.error(format("Record publishing exception : key={0}", key), e);
-                    });
+                    });*/
+
+                    kafkaTemplate.send(topicName, key, bus);
+                    LOG.info(format("Sent message with key={0}", key));
 
                 });
-
 
             });
 
         } catch (Exception e) {
             LOG.error(format("Producer Exception for busStopCode={0}", messageToPublish.getBusStopCode()), e);
-        } finally {
-            producer.flush();
-            producer.close();
         }
 
     }
