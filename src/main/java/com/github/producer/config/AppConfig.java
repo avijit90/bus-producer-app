53822f42b743f871a621abf2d5652a3957f07922
avijit90 <sepiaavi@gmail.com>
2019-10-03 00:19:07 +0800

    Config added for spring-templates.

--- src/main/java/com/github/producer/config/AppConfig.java
+++ src/main/java/com/github/producer/config/AppConfig.java
@@ -1,17 +1,45 @@
 package com.github.producer.config;
 
+import com.github.producer.model.Bus;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
 
+    @Value("${app.kafka.bootstrapServer: localhost:9092}")
+    private String bootstrapServer;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ProducerFactory<String, Bus> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Bus> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
 }
