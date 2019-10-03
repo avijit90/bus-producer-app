57a485f27990684a468d24ed0d5bdb6346599dfa
Avijit Vaidya <avijit.vaidya@finastra.com>
2019-10-03 18:35:34 +0800

    Producer success and failure callbacks added.

--- src/main/java/com/github/producer/kafka_producer/Producer.java
+++ src/main/java/com/github/producer/kafka_producer/Producer.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
 
@@ -31,29 +34,31 @@ public class Producer {
         try {
 
             String busStopCode = messageToPublish.getBusStopCode();
-
             messageToPublish.getServices().stream().forEach(service -> {
 
-                String prefix = join(asList(busStopCode, service.getServiceNo(), "B"), "_");
-
-                List<Bus> buses = asList(service.getNextBus(), service.getNextBus2(), service.getNextBus3());
+                String key = join(asList(busStopCode, service.getServiceNo()), "_");
+                List<Bus> buses = service.getBusList();
                 buses.stream().forEach(bus -> {
 
-                    String key = prefix.concat(Integer.toString(buses.indexOf(bus)));
+                    ListenableFuture<SendResult<String, Bus>> listenableFuture = kafkaTemplate.send(topicName, key, bus);
+                    listenableFuture.addCallback(new ListenableFutureCallback<SendResult<String, Bus>>() {
 
-                   /* kafkaTemplate.send(record, (recordMetadata, e) -> {
-                        if (e == null) {
-                            LOG.info("Received new metadata");
-                            LOG.info(format("Topic : {0}", recordMetadata.topic()));
-                            LOG.info(format("Partition : {0}", recordMetadata.partition()));
-                            LOG.info(format("Offset : {0}", recordMetadata.offset()));
-                            LOG.info(format("Timestamp : {0}", recordMetadata.timestamp()));
-                        } else
+                        @Override
+                        public void onFailure(Throwable e) {
                             LOG.error(format("Record publishing exception : key={0}", key), e);
-                    });*/
+                        }
 
-                    kafkaTemplate.send(topicName, key, bus);
-                    LOG.info(format("Sent message with key={0}", key));
+                        @Override
+                        public void onSuccess(SendResult<String, Bus> result) {
+                            LOG.info("-----------------------------------------------------------");
+                            LOG.info(format("Message with key={0}", key));
+                            LOG.info(format("Topic : {0}", result.getRecordMetadata().topic()));
+                            LOG.info(format("Partition : {0}", result.getRecordMetadata().partition()));
+                            LOG.info(format("Offset : {0}", result.getRecordMetadata().offset()));
+                            LOG.info(format("Timestamp : {0}", result.getRecordMetadata().timestamp()));
+                            LOG.info("-----------------------------------------------------------");
+                        }
+                    });
 
                 });
 
